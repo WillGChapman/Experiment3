1ac1c669483ba9077ba0e992e4493e6dff64067d
Will Chapman <willgchapman@gmail.com>
2019-07-16 23:51:18 +0100

    initial script for computing deviance between simulates and recorded AUCs

---
+++ simdeviance.R
@@ -0,0 +1,12 @@
+# script for fun thing with simulated deviance.
+
+#Pseudo: - Simulate N trajectories
+# - Compute N AUCs
+# - Fit kernel density to N simulated AUCs: 
+#   dens<-density(AUC,n=1024,from=min,to=max,na.rm=TRUE)
+#   x<-fixDens$x
+#   y<-fixDens$y
+#   - Interpolate the (x,y) density at your observed AUCs:
+#     approx(observed,x,y,method="linear")
+#   - Compute deviance: log, sum, times -2
+
