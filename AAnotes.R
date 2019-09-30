f68dfe2177bc969ad1ef34925f70416669cff2fb
Will Chapman <willgchapman@gmail.com>
2019-09-30 22:57:17 +0100

    notes for tomorrow

---
+++ AAnotes.R
@@ -0,0 +1,11 @@
+#Oct 1 2019
+
+#check "realistic" movement speed from data (mean velocity probably fine)
+
+#play with parameters of the model to match data (make straighter lines, force occasional mistakes)
+
+#then run some optimiser to get something reasonable
+
+#goal is to have a model against which to compare submovement model
+
+#if it's never going to work it will hopefully be obvious soon
