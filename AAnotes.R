0695beeb829e22d7c86c377119508a324e51e884
WillGChapman <willgchapman@gmail.com>
2019-10-03 17:22:52 +0100

    thursday evening

--- AAnotes.R
+++ AAnotes.R
@@ -36,14 +36,44 @@ plot(trajs$effectorpos[[longestreach]][1,], trajs$effectorpos[[longestreach]][2,
 
 #gridsearch decbound parameter from 1-200 to see if anything changes
 decs <- numeric()
-for (db in 51:200){
-  trajd <- trajectories(walks, decbound = db, suppresscount = TRUE)
+for (db in 1:50){
+  walkd <- ranwalks(5000,500,1,1, suppress.count = TRUE)
+  pb$tick()
+  trajd <- trajectories(walkd, decbound = db, suppresscount = TRUE)
   decs <- append(decs, sum(trajd$decision==1))
 }
 
-#up to a decision boundary of 20, number of errors increases and asymptotes at 101
+plot(decs, type='l')
 
+#up to a decision boundary of 20 (mean drift and variance of ), number of errors increases and asymptotes at 101 (with the same random walk dataset), which is interesting.
+#perhaps this means that the decision variable is wrong for enough time that the incorrect destination is reached.
+#the feedback gain on this probably helps.
+
+#another gridsearch was run with ranwalks rerun every time. By about b=10, error rate starts to approach 10%
 #then run some optimiser to get something reasonable
+
+#trying again with a walks =5000, to see if we get a more stable estimate
+#not much tbh.
+
+#OK now - take a look at the trajectories generated. Keep the decision boundary at 10.
+
+walks <- ranwalks(1000, 500, 1,1)
+
+trajs <- trajectories(walks, decbound = 10, suppresscount = F)
+
+#lets take a peek at the paths
+
+plot(trajs$effectorpos[[1]][1,], trajs$effectorpos[[1]][2,], type='l', 
+     col = alpha(colour = 'black', alpha = 0.25),
+     xlim = c(-15,15))
+for (trial in 1:length(trajs[[1]]))
+{
+  x <- 1
+  if (trajs$decision[[trial]]==1){x = -1}
+  lines(x*trajs$effectorpos[[trial]][1,], trajs$effectorpos[[trial]][2,], type='l', col = alpha(colour = 'black', alpha = 0.25))
+}
+
+#a higher decision boundary doesn't affect the rate at which incorrect decisions are made.
 
 #goal is to have a model against which to compare submovement model
 
